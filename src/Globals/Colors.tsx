3e0ed46a46071413e8c6b0ff3ade6710b953d888
Amr Essam <amr_esam@ymail.com>
2023-02-23 22:52:58 +0200

    color changes

--- src/Globals/Colors.tsx
+++ src/Globals/Colors.tsx
@@ -1,9 +1,18 @@
 export const Colors = {
-  backgroundDarker: '#000000',
-  backgroundDark: '#3E3E3E',
-  backgroundLight: '#e2e2e2',
+  backgroundDarker: '#000814',
+  backgroundDark: '#2b2d42',
+  backgroundLight: '#edf2f4',
   backgroundAccent: '#ef233c',
-  textPrimaryDark: '#242038',
-  textPrimaryLight: '#e2e2e2',
+  textPrimaryDark: '#2b2d42',
+  textPrimaryLight: '#edf2f4',
   textAccent: '#ef233c',
+  transparent: 'transparent',
+  // <color name="background_light"> edf2f4</color>
+  // <color name="background_dark"> 2b2d42</color>
+  // <color name="background_darker"> 000814</color>
+  // <color name="text_light"> edf2f4</color>
+  // <color name="text_dark"> 2b2d42</color>
+  // <color name="text_darker"> 000814</color>
+  // <color name="background_accent"> 2660a4</color>
+  // <color name="text_accent"> 1789fc</color>
 };
